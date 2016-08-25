a8e7ab21ab12c759bca4454ab72302a969a1f5cf
nukeop <alek.poczta@gmail.com>
2016-08-26 00:25:48 +0100

    More hooks, expanded constructor

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,24 +1,28 @@
 class Plugin(object):
     """Plugin template - all plugins must inherit from this
     """
-    def __init__(self):
-        pass
+    def __init__(self, bot):
+        """Constructor. Receives a reference to the bot so that the plugins are
+        able to call its various functions. All plugins need to call this
+        constructor.
+        """
+        self.bot = bot
 
     @property
     def description(self):
         """Description of this plugin's functionality - will be shown to users
-    who ask for a list of plugins.
+        who ask for a list of plugins.
         """
         raise NotImplementedError("Plugin should never be created directly")
 
     @property
     def long_desc(self):
-    """Long description of this plugin's functionality - should be exhaustive
-    and describe all nuances of using it. This will be shown to users who
-    request help about this particular plugin, which means it should be as long
-    as needed.
-    """
-    raise NotImplementedError("Plugin should never be created directly")
+        """Long description of this plugin's functionality - should be
+        exhaustive and describe all nuances of using it. This will be shown to
+        users who request help about this particular plugin, which means it should
+        be as long as needed.
+        """
+        raise NotImplementedError("Plugin should never be created directly")
 
     def init_hook(self):
         """This will be registered and called exactly once when the bot
@@ -39,5 +43,10 @@ class Plugin(object):
         It will receive the group id, the user's steamid and the sent message,
         in the future possibly also other arguments.
         """
+        raise NotImplementedError("Plugin should never be created directly")
 
+    def enter_group_chat_hook(self, groupid):
+        """This will be registered and called whenever the bot enters a group
+        chat. It will receive the group id.
+        """
         raise NotImplementedError("Plugin should never be created directly")
