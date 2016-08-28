914e143a82bffb836d859a19fd8b83df60a8e38b
nukeop <alek.poczta@gmail.com>
2016-08-28 23:56:16 +0100

    Minor improvements

--- plugins/help.py
+++ plugins/help.py
@@ -13,8 +13,8 @@ class Help(plugin.Plugin):
 
     @property
     def long_desc(self):
-        return "!help - shows a list of all available plugins and what they"
-        "do.\n !help <command> - shows detailed information about a command."
+        return ("!help - shows a list of all available plugins and what they"
+        " do.\n !help <command> - shows detailed information about a command.")
 
     def private_chat_hook(self, steamid, message):
         if message.startswith(self.command):
@@ -25,6 +25,9 @@ class Help(plugin.Plugin):
                 self.bot.user.send_msg(steamid, self.build_help_list())
 
     def build_help_list(self):
+        """Builds a list of short descriptions of plugins along with their
+        names.
+        """
         result = "\n"
         for plugin in self.bot.plugins:
             result += "{}\n\t{}\n".format(type(plugin).__name__,
@@ -32,10 +35,10 @@ class Help(plugin.Plugin):
         return result
 
     def get_long_desc(self, name):
+        """Looks for a plugin and gets its long description.
+        """
         result = None
         for plugin in self.bot.plugins:
-            print name
-            print type(plugin).__name__
             if type(plugin).__name__ == name:
                 result = plugin.long_desc
                 break
