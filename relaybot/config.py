5e073b7082e75502a6053354267c7c0bacb38e02
nukeop <alsw@protonmail.com>
2016-11-02 19:00:17 +0000

    Remove print statements from config

--- relaybot/config.py
+++ relaybot/config.py
@@ -3,6 +3,4 @@ import json
 import os
 
 config_path = os.path.join(os.getcwd(),'relaybot','config.json')
-print config_path
-print ''.join(open(config_path, 'r').readlines())
 config = json.loads(''.join(open(config_path, 'r').readlines()))
