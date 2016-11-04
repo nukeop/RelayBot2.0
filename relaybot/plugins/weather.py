8f381b171e71ed1fabfa708004e375d169721255
WaltonSimons <masteroskar@gmail.com>
2016-11-04 14:41:23 +0100

    minor fixes

--- relaybot/plugins/weather.py
+++ relaybot/plugins/weather.py
@@ -28,37 +28,15 @@ class Weather(plugin.Plugin):
             "!weather": "Shows basic weather conditions from OpenWeatherMap"
         }
 
-    def init_hook(self):
-        """This will be registered and called exactly once when the bot
-        initializes itself.
-        """
-        pass
-
     def private_chat_hook(self, steamid, message):
         if message.startswith(self.command):
             self.bot.user.send_msg(steamid, self.weather(
                 ' '.join(message[:-1].split(' ')[1:])))
-        pass
 
     def group_chat_hook(self, groupid, userid, message):
         if message.startswith(self.command):
             self.bot.user.send_msg(groupid, self.weather(
                 ' '.join(message[:-1].split(' ')[1:])))
-        pass
-        pass
-
-    def enter_group_chat_hook(self, groupid):
-        pass
-
-    def user_entered_hook(self, groupid, userid):
-        """Called whenever a user enters a group chat the bot is in.
-        """
-        pass
-
-    def user_left_hook(self, groupid, userid):
-        """Called whenever a user leaves a group chat the bot is in.
-        """
-        pass
 
     def weather(self, location):
         r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&APPID='+self.apikey).text
